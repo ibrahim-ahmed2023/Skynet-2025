a3b422f07d5ed1cc97207964ed28f7b1fd620b4d
ibrahim ahmed <ibrahima.abdelwahab@gmail.com>
2025-03-15 19:14:37 +0200

    shoppingCartAngular

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -6,12 +6,16 @@ import { TestErrorComponent } from './features/test-error/test-error.component';
 import { NotFoundComponent } from './shared/compomemts/not-found/not-found.component';
 import { ServerErrorComponent } from './shared/compomemts/server-error/server-error.component';
 import { ContactsComponent } from './features/contacts/contacts.component';
+import { CartComponent } from './features/cart/cart.component';
+import { CheckoutComponent } from './features/checkout/checkout.component';
 
 export const routes: Routes = [
     {path: '' , component: HomeComponent},
     {path: 'shop' , component: ShopComponent},
     {path: 'shop/:id' , component: ProductDetailsComponent},
+    {path: 'cart' , component: CartComponent},
     {path: 'test-error' , component: TestErrorComponent},
+    {path: 'checkout', component: CheckoutComponent},
     {path: 'not-found' , component: NotFoundComponent},
     {path: 'server-error' , component: ServerErrorComponent},
     {path: 'contacts' , component: ContactsComponent},
